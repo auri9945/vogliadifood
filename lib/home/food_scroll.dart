f6b966031960b650eb4ccdff191da4fb43e7d5ba
elisa <so.elisa9@gmail.com>
2023-12-20 18:39:55 +0100

    aggiunta elementi fino alle foto sotto

--- lib/home/food_scroll.dart
+++ lib/home/food_scroll.dart
@@ -1,10 +1,12 @@
-
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vogliadifood/utils/colors.dart';
+import 'package:vogliadifood/utils/dimensions.dart';
 import 'package:vogliadifood/widgets/Icon_and_text_widget_dart.dart';
 import 'package:vogliadifood/widgets/big_text.dart';
 import 'package:vogliadifood/widgets/small_text.dart';
+/*import 'package:get/get.dart';*/
 
 
 
@@ -19,25 +21,100 @@ class FoodScroll extends StatefulWidget {
 
 class _FoodScrollState extends State<FoodScroll> {
   PageController pageController = PageController(viewportFraction: 0.85);
-
+  var _currPageValue=0.0;
+  /*double _height=Dimensions.pageViewContainer;*/
+  @override
+  void initState(){
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue= pageController.page!;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-    height: 320,
-      child: PageView.builder(
-        controller: pageController,
-        itemCount: 3,//per pagine di scroll
-        itemBuilder: (context, position){
-          return _buildPageItem(position);
-        }),
+    return Column(
+      children: [
+        Container(
+          height: 320,
+          child: PageView.builder(
+              controller: pageController,
+              itemCount: 3,//per pagine di scroll
+              itemBuilder: (context, position){
+                return _buildPageItem(position);
+              }),
+        ),
+        new DotsIndicator(
+        dotsCount: 5,
+        position: _currPageValue,
+        decorator: DotsDecorator(
+          activeColor: AppColors.Grigio,
+          size: const Size.square(9.0),
+          activeSize: const Size(18.0, 9.0),
+          activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+        ),
+        ),
+
+     //popular text
+        SizedBox(height: Dimensions.height30,),
+        Container(
+          margin: EdgeInsets.only(left:Dimensions.width30),
+          child: Row(
+            children: [
+              BigText(text: "popular"),
+              SizedBox(width: Dimensions.width10,),
+              Container(
+                child: BigText(text: ".", color: Colors.black26,),
+              ),
+              SizedBox(width: Dimensions.width10,),
+              Container(
+                child: SmallText(text: "alcuni ristoranti:",),
+              )
+
+            ],
+          ),
+        ),
+        //lista di cibo e immagini
+        Container(
+          height: 700,
+          child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index){
+                  return Container(
+                      margin: EdgeInsets.only(left:Dimensions.width20,right: Dimensions.width20),
+                      child: Row(
+                        children: [
+                          Container(
+                            width:120,
+                            height: 120,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                                color: Colors.white38,
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                        "assets/image/cibo0.jpg"
+                                    )
+                                )
+                            ),
+                          )
+                        ],
+                      )
+                  );
+                })
+        )
+
+      ],
     );
+
   }
+
   Widget _buildPageItem(int index) {
     return  Stack(
         children:[
           Container(
-          height: 220,
+          height: Dimensions.pageViewContainer,
           margin: EdgeInsets.only(left:5, right: 5),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
@@ -50,7 +127,7 @@ class _FoodScrollState extends State<FoodScroll> {
          Align(
            alignment: Alignment.bottomCenter,
            child: Container(
-              height: 150,
+              height: Dimensions.pageViewTextContainer,
               margin: EdgeInsets.only(left:40, right: 40, bottom: 30),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30),
---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+class Dimensions {
+
+static double screenHeight = Get.context!.height;
+static double screenWidth = Get.context!.width;
+
+static double pageView=screenHeight/2.64;
+static double pageViewContainer= screenHeight/3.84;
+static double pageViewTextContainer= screenHeight/7.03;
+
+static double height10 = screenHeight/84.4;
+static double height20 = screenHeight/42.2;
+static double height30 = screenHeight/22.2;
+
+static double width10 = screenWidth/84.4;
+static double width20 = screenWidth/42.2;
+static double width30 = screenWidth/22.2;
+
+//radius
+static double radius15 = screenHeight/57.6;
+static double radius20 = screenHeight/42.2;
+static double radius30 = screenHeight/28.13;
+
+
+}
